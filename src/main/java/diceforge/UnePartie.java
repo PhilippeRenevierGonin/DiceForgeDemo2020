cd3f43cb3f03f7535959a3f45eb65a20d67129a2
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2020-01-21 17:34:08 +0100

    pris en compte de l'encodage. Again, this closes #4

--- src/main/java/diceforge/UnePartie.java
+++ src/main/java/diceforge/UnePartie.java
@@ -1,8 +1,17 @@
 package diceforge;
 
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class UnePartie {
 
     public static final void main(String [] args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("début de la partie");
     }
 }
